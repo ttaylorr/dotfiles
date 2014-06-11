b63bbe1c8ef7e7d4941b63a1489bf588097e90d2
Taylor Blau <me@ttaylorr.com>
2014-06-11 17:09:36 -0400

    More verbos output logging for system

--- system/install.sh
+++ system/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function main() {
+  info "Installing system things (mainly .bashrc)"
   init_tmp_bashrc
   source "./aliases.sh"
   copy_bashrc
@@ -9,15 +10,22 @@ function main() {
 
 function init_tmp_bashrc() {
   cleanup
+  info "Creating new /tmp directory"
   mkdir "./tmp"
   touch "./tmp/.bashrc"
 }
 
 function copy_bashrc() {
   cp "./tmp/.bashrc" "$HOME/.bashrc"
+  if [ $? -eq 0 ]; then
+    success "Copied over the new .bashrc!"
+  else
+    fail "Could not write out the new .bashrc"
+  fi
 }
 
 function cleanup() {
+  info "Cleaning out old dust"
   `rm -rf "./tmp"`
 }
 
