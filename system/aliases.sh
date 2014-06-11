e231c04e036749afcbf828f93319d3018be64b40
Taylor Blau <me@ttaylorr.com>
2014-06-11 17:11:40 -0400

    Append aliases, write out ls with color

--- system/aliases.sh
+++ system/aliases.sh
@@ -2,10 +2,11 @@
 
 function main() {
   write_alias "g" "git"
+  write_alias "ls" "ls -G"
 }
 
 function write_alias() {
-  echo "alias $1='$2'" > ./tmp/.bashrc
+  echo "alias $1='$2'" >> ./tmp/.bashrc
 }
 
 main
