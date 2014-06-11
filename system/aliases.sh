00adab45d1e687a1cc81f82add623c489c0a4b7e
Taylor Blau <me@ttaylorr.com>
2014-06-11 17:19:43 -0400

    Missing whitespace

--- system/aliases.sh
+++ system/aliases.sh
@@ -7,7 +7,7 @@ function main() {
 
 function write_alias() {
   echo "alias $1='$2'" >> ./tmp/.bashrc
-  if [ $? -eq 0]; then
+  if [ $? -eq 0 ]; then
     info "Aliased '$1' to '$2' in new .bashrc"
   else
     fail "Could not make alias from '$1' to '$2'"
