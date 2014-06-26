97ca1f6a2b12bf51115201da0c3e9245a9e9f907
Taylor Blau <me@ttaylorr.com>
2014-06-26 00:08:05 -0400

    run brew doctor after homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,8 +13,18 @@ function install_homebrew() {
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
   if [ $? -eq 0 ]; then
     success "Homebrew installed!"
+    brew_doctor()
   else
     fail "Could not install homebrew :("
+  fi
+}
+
+function brew_doctor() {
+  `brew doctor`
+  if [ $? -eq 0]; then
+    success "Doctored it up!"
+  else
+    fail "Could not run brew doctor :("
   fi
 }
 
