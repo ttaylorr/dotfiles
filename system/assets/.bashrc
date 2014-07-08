7ec8c1040f13cd1835af18be20089f8c4477ab19
Taylor Blau <me@ttaylorr.com>
2014-07-08 15:57:57 -0400

    Install vim and mvim

---
+++ system/assets/.bashrc
@@ -0,0 +1,4 @@
+alias g='git'
+alias ls='ls -G'
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 function main() {
+  install_vim
+  install_mvim
+
   cp ./assets/.vimrc ~/.vimrc
   success 'Copied over .vimrc!'
 }
+
+function install_vim() {
+  info 'Installing vim...'
+  `brew install vim`
+  success 'Installed vim!'
+}
+
+function install_mvim() {
+  info 'Installing mvim...'
+  `brew install macvim`
+  success 'Installed mvim!'
+}
+
 
 main
