c987a4fcf2962194c6b244a39cb48b089355f324
Taylor Blau <me@ttaylorr.com>
2015-01-19 21:19:42 -0500

    Put ~/.bin on the path in ~/.bashrc

--- system/install.sh
+++ system/install.sh
@@ -5,6 +5,7 @@ function main() {
   init_tmp_bashrc
   source "./aliases.sh"
   echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ./tmp/.bashrc
+  echo "export PATH='$PATH:$HOME/.bin'" >> ./tmp/.bashrc
   copy_bashrc
   cleanup
 }
