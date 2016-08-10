5ca4ab5f5b1dbab09951d924ce38da2df229b33c
Taylor Blau <me@ttaylorr.com>
2016-08-10 11:05:51 -0600

    karabiner: left control to escape, or control

--- karabiner/import.sh
+++ karabiner/import.sh
@@ -2,6 +2,7 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.controlL2controlL_escape 1
 $cli set remap.doublepresscommandQ 1
 $cli set remap.pointing_relative_fn_to_scroll 1
 $cli set repeat.initial_wait 250
