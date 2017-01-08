fdfdae609e2992d7281ccdaa7c9ff2be3b8db196
Taylor Blau <me@ttaylorr.com>
2017-01-08 12:30:23 -0700

    karabiner: disable doublepresscommandQ

--- karabiner/import.sh
+++ karabiner/import.sh
@@ -4,7 +4,6 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set parameter.keytokey_delayed_action_timeout 35
 $cli set remap.controlL2controlL_escape 1
-$cli set remap.doublepresscommandQ 1
 $cli set remap.pointing_relative_fn_to_scroll 1
 $cli set repeat.initial_wait 250
 $cli set repeat.wait 50
