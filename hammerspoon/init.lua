b839b818e7c28b56bc9042f66c4fc918a8c4847e
Taylor Blau <me@ttaylorr.com>
2017-01-08 12:29:54 -0700

    hammerspoon: implement 'quit' module

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,3 +3,5 @@ hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send(
 hs.hotkey.bind({'ctrl'}, '`', function()
   hs.reload()
 end)
+
+require('quit')
---
+++ hammerspoon/quit.lua
@@ -0,0 +1,29 @@
+-- Module: quit
+--
+-- Re-binds Cmd+Q to quit frontmost application after two presses to prevent
+-- accidental closing.
+--
+-- Source: https://github.com/raulchen/dotfiles.
+
+-- modal is the quitting modal. It holds the inner-binding to Cmd-Q which wll
+-- actually invoke the quit.
+local modal = hs.hotkey.modal.new('cmd', 'q')
+
+-- modal:entered uses a timer to exit the modal one second after entering.
+function modal:entered()
+  hs.timer.doAfter(1, function() modal:exit() end)
+end
+
+-- quit selects the frontmost application and selects the first menu item
+-- matching "Quit", the exits the modal.
+local function quit()
+  local app = hs.application.frontmostApplication()
+  local res = app:selectMenuItem("^Quit.*$")
+
+  modal:exit()
+end
+
+-- inner-binding for quitting an application
+modal:bind('cmd', 'q', quit)
+-- inner-binding for leaving the modal
+modal:bind('', 'escape', function() modal:exit() end)
