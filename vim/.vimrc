ttaylorr/dotfiles

commit 4b49ed1f2c65d9aba7901b2c32219469ca24b432

vim/.vimrc: bind <c-B> to running 'tig blame' at the current line

vim/.vimrc
@@ -113,6 +113,8 @@ endfunction
 command! -nargs=* Git :call MaybeInlineCommand("git always <args>")
 command! -nargs=* Gitc :call MaybeInlineCommand("git.compile always <args>")
 
+nnoremap <c-B> :execute '!TIG_SCRIPT=<(echo :' . line(".") . ') tig blame %'<cr> | redraw!
+
 nnoremap <silent> <c-h> <c-W><c-h>
 nnoremap <silent> <c-j> <c-W><c-j>
 nnoremap <silent> <c-k> <c-W><c-k>

commit b240527bb00886214621540e84b43bdd6f4ac7b3

vim/.vimrc: avoid broken `redraw!` after invoking `tig`
Commit 4b49ed1 (vim/.vimrc: bind <c-B> to running 'tig blame' at the
current line, 2024-06-17) introduced a subtle bug in the vimrc that
caused the terminal to improperly dispose of the alternate buffer,
leaving garbage on the screen.

I'm not entirely sure why this happened. To debug, I did the following:

 * Run Vim without any additional configuration via "vim --clean".

 * After confirming that the issue no longer reproduced, re-launched Vim
   without "--clean", but only after commenting out the entirety of my
   vimrc.

 * Un-comment my vimrc section by section until I isolated the issue.

The "| redraw!" part of this remap was written to prevent the user from
having to whack "enter" after quitting the tig process. ChatGPT
suggested a couple of alternatives, but neither of them worked. Hence,
"<cr><cr>", which still waits for an "enter" key, but presses it
automatically.

I can't shake the feeling that this is a pretty gross solution, but it
seems to do the trick and avoids polluting my terminal with the
(partial) contents of the alternate buffer.

Signed-off-by: Taylor Blau <me@ttaylorr.com>

vim/.vimrc
@@ -113,7 +113,7 @@ endfunction
 command! -nargs=* Git :call MaybeInlineCommand("git always <args>")
 command! -nargs=* Gitc :call MaybeInlineCommand("git.compile always <args>")
 
-nnoremap <c-B> :execute '!TIG_SCRIPT=<(echo :' . line(".") . ') tig blame %'<cr> | redraw!
+nnoremap <c-B> :execute '!TIG_SCRIPT=<(echo :' . line(".") . ') tig blame %'<cr><cr>
 
 nnoremap <c-n> :cn<cr>
 nnoremap <c-m> :cp<cr>